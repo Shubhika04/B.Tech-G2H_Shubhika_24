c4224ea9c7e420350ddab19c85e51116129d7fcf
Shubhika04 <shubhikasuman.avhpm@gmail.com>
2026-02-13 11:50:11 +0530

    Update equal.c

--- equal.c
+++ equal.c
@@ -1,38 +1,5 @@
-// // #include<stdio.h>
-// // int main()
-// // {
-// // float km, miles;
-// // printf(" Enter distance in kilometers: \n");
-// // scanf("%f", &km);
-// // miles = km*0.621;
-// // printf("Miles = %f\n", miles);
-// // printf(" Enter the distance in miles: \n ");
-// // scanf("%f", &miles);
-// // km = miles * 1.609;
-// // printf("Kilometers = %f\n", km);
 
-// // return 0;
-// // }
 
-// #include <stdio.h>
-// int main()
-// {
-//     char q;
-//     printf(" Enter a character= ");
-//     scanf("%c", &q);
-//     if (q == 'a' || q == 'e' || q == 'i' || q == 'o' || q == 'u' ||
-//         q == 'A' || q == 'E' || q == 'I' || q == 'O' || q == 'U' )
-//     {
-//         printf("character is vowel");
-//     }
-//     else
-//     {
-
-//         printf(" character is consonant");
-//     }
-
-//     return 0;
-// }
 #include<stdio.h>
 int main() {
     int a,b;
@@ -47,4 +14,5 @@ int main() {
     printf("the numbers are not equal \n");
     }
 return 0;
+
 }
