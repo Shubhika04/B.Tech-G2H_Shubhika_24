d39c330db77eb2093e9842a6af2be34c8bde9aef
Shubhika04 <shubhikasuman.avhpm@gmail.com>
2026-02-21 00:23:34 +0530

    Add files via upload

---
+++ fahrenheit-centigrade.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    float celcius, fahrenheit;
+    printf("Enter temp in centigrade: \n");
+    scanf("%f", &celcius);
+     fahrenheit= (celcius * 9/5)+32;
+      printf("temp in fahrenheit= %f", fahrenheit);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
