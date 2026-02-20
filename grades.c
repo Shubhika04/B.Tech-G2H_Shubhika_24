1d7fd89c9873bfda80e5060acd996f6d6daf4399
Shubhika04 <shubhikasuman.avhpm@gmail.com>
2026-02-20 12:11:40 +0530

    Add files via upload

---
+++ grades.c
@@ -0,0 +1,30 @@
+
+ 
+#include<stdio.h>
+ int main() {
+    float M1, M2, M3, M4, M5;
+    float sum, percentage;
+    
+    printf("Enter the Marks of 5 Subjects : ");
+    scanf("%f %f %f %f %f", &M1, &M2, &M3, &M4, &M5);
+ 
+    sum = M1 + M2 + M3 + M4 + M5;
+    percentage = (sum / 500) * 100;
+    
+    printf("\n Total Marks = %.2f", sum);
+    printf("\n Overall Percentage  = %.2f%% \n", percentage);
+    
+    if (percentage>=90 && percentage<=100){
+      printf(" Grade A");
+    }
+         if (percentage>=80 && percentage<90){
+      printf(" Grade B");
+    }
+        if (percentage>=60 && percentage<80){
+      printf(" Grade C");
+    }
+     if (percentage<60) {
+      printf(" Grade D");
+    }
+    return 0;
+ }
