7d2061e2afe1c5dbe6743b928488864e13a64488
Shubhika04 <shubhikasuman.avhpm@gmail.com>
2026-03-24 00:43:47 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int x,a,b, result=1;
+printf("enter base: \n");
+scanf("%d", &a);
+printf("enter exponent: \n");
+scanf("%d", &b);
+x=b;
+while(b!=0)
+{
+result= a*result;
+--b;
+}
+printf("%d raised to the power %d is %d", a,b,result);
+return 0;
+}  
