fe888018a4092184a1cacfc3be87fad736f85fa7
Shubhika04 <shubhikasuman.avhpm@gmail.com>
2026-04-03 09:44:36 +0530

    Update diagonal elements sum.c

--- diagonal elements sum.c
+++ diagonal elements sum.c
@@ -4,11 +4,14 @@ int a[10][10],m,n,i,j,s1=0,s2=0;
 printf("enter the dim of matrix:\n");
 scanf("%d%d",&m,&n);
 printf("enter the elements of matrix:\n");
+
 for(i=0;i<m;i++) {
     for(j=0;j<n;j++) {
         scanf("%d",&a[i][j]);
     }
 }
+if(m==n)
+{
 for(i=0;i<m;i++) {
     s1=s1+a[i][i];
 }
@@ -17,5 +20,9 @@ for(i=0;i<m;i++) {
 }
 printf("sum of main diagonal elements is %d\n",s1);
 printf("sum of off diagonal elements is %d\n",s2);
+}
+else{
+printf("required sum is not possible");
+}
 return 0;
 }
